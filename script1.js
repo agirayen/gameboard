47a686e67cb3df03f56f1249b10a589330fa0f82
Agnita <ageetha2@pdx.edu>
2023-02-08 10:42:50 -0800

    updated script1 file

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
 
 function shuffle(){
-  var const = document.getElementById("container");
+  var cont = document.getElementById("container");
   var hexArr = cont.getElementsByClassName("hex")
   var elementsArray = Array.prototype.slice.call(hexArr);
 
